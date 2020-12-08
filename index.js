5e4def5e2d4736a6202beedb96d3b4134e9b5672
whichxjy <whichxjy@gmail.com>
2020-12-08 15:16:25 +0800

    Update

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const puppeteer = require("puppeteer");
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("http://10.8.11.170/web/index.htm");
+    await page.goto("http://10.8.11.170/web/images/101/101index.htm");
+
+    // Type search word.
+    const searchSelector = "input[name='searchword1']";
+    await page.waitForSelector(searchSelector);
+    await page.type(searchSelector, "Hello");
+
     await page.screenshot({ path: "hello.png" });
 
     await browser.close();
