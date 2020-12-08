0b3da17ddb3a318fde7356d756c1804957805d41
whichxjy <whichxjy@gmail.com>
2020-12-08 15:40:42 +0800

    Update

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const puppeteer = require("puppeteer");
 
+class Worker {
+    constructor(title, link) {
+        this.title = title;
+        this.link = link;
+    }
+}
+
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -16,13 +23,18 @@ const puppeteer = require("puppeteer");
         page.click("input[title='提交检索']"),
     ]);
 
+    // Build worker list.
+    const workerList = [];
+
     // Get item list.
     const itemList = await page.$$(".div_rmrb-outlinetitle");
     for (let item of itemList) {
-        let link = await item.$eval("a.ab18", el => el.getAttribute("href"));
         let title = await item.$eval("a.ab18", el => el.innerHTML);
-        console.log(title, link);
+        let link = await item.$eval("a.ab18", el => el.getAttribute("href"));
+        workerList.push(new Worker(title, link));
     }
+
+    console.log(workerList);
 
     await page.screenshot({ path: "hello.png" });
 
